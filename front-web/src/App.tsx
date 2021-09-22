28efa8c89459abc0ba30eef171fded74f1f1ea24
fhilips <fhilips.souza@gmail.com>
2021-09-22 10:53:16 -0300

    filter, pie chart card containers responsive

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css';
+import Filter from './components/filter';
 import Header from './components/header';
+import PieChartCard from './components/pie-chart-card';
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Filter />
+      <PieChartCard />
+    </div>
+  );
 }
 
 export default App;
---
+++ front-web/src/components/pie-chart-card/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+const PieChartCard = () => {
+  return (
+    <header className="pie-chart-card-container base-card">
+      <div className="pie-chart-card-title-container">
+        <h1 className="total-sale-value">R$ 746.484,00</h1>
+        <span className="total-sales-text">Total de vendas</span>
+      </div>
+    </header>
+  );
+};
+
+export default PieChartCard;
